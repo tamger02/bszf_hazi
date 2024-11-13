815149e2684d12fd6ea3733529b464f7be5fc6f4
dburanszki <112810950+dburanszki@users.noreply.github.com>
2024-11-13 11:10:51 +0100

    game fileok letrehozasa, object fajlok atpakolasa

---
+++ snake/game.h
@@ -0,0 +1,16 @@
+/*
+ * game.h
+ *
+ *  Created on: 13 Nov 2024
+ *      Author: dbura
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+#include "Objects.h"
+
+kigyo snake; //kigyo peldanyositasa;
+uint16_t score;
+alma apple;
+
+#endif /* GAME_H_ */
